ac20a8917f4097faa3bd48b8298bae8202e78a6d
Lawrence Forman <me@merklejerk.com>
2020-10-28 20:58:20 -0400

    `@0x/utils`: drop revert error definitions

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -27,32 +27,3 @@ export {
     AnyRevertError,
 } from './revert_error';
 export { fromTokenUnitAmount, toTokenUnitAmount } from './token_utils';
-
-export import BrokerRevertErrors = require('./revert_errors/broker/revert_errors');
-export import CoordinatorRevertErrors = require('./revert_errors/coordinator/revert_errors');
-export import ExchangeForwarderRevertErrors = require('./revert_errors/exchange-forwarder/revert_errors');
-export import LibMathRevertErrors = require('./revert_errors/exchange-libs/lib_math_revert_errors');
-export import ExchangeRevertErrors = require('./revert_errors/exchange/revert_errors');
-export import LibAssetDataTransferRevertErrors = require('./revert_errors/extensions/lib_asset_data_transfer_revert_errors');
-export import MixinWethUtilsRevertErrors = require('./revert_errors/extensions/mixin_weth_utils_revert_errors');
-export import FixedMathRevertErrors = require('./revert_errors/staking/fixed_math_revert_errors');
-export import StakingRevertErrors = require('./revert_errors/staking/staking_revert_errors');
-export import AuthorizableRevertErrors = require('./revert_errors/utils/authorizable_revert_errors');
-export import LibAddressArrayRevertErrors = require('./revert_errors/utils/lib_address_array_revert_errors');
-export import LibBytesRevertErrors = require('./revert_errors/utils/lib_bytes_revert_errors');
-export import OwnableRevertErrors = require('./revert_errors/utils/ownable_revert_errors');
-export import ReentrancyGuardRevertErrors = require('./revert_errors/utils/reentrancy_guard_revert_errors');
-export import SafeMathRevertErrors = require('./revert_errors/utils/safe_math_revert_errors');
-
-export const ZeroExRevertErrors = {
-    Common: require('./revert_errors/zero-ex/common_revert_errors'),
-    Proxy: require('./revert_errors/zero-ex/proxy_revert_errors'),
-    SimpleFunctionRegistry: require('./revert_errors/zero-ex/simple_function_registry_revert_errors'),
-    Ownable: require('./revert_errors/zero-ex/ownable_revert_errors'),
-    Spender: require('./revert_errors/zero-ex/spender_revert_errors'),
-    TransformERC20: require('./revert_errors/zero-ex/transform_erc20_revert_errors'),
-    Wallet: require('./revert_errors/zero-ex/wallet_revert_errors'),
-    MetaTransactions: require('./revert_errors/zero-ex/meta_transaction_revert_errors'),
-    SignatureValidator: require('./revert_errors/zero-ex/signature_validator_revert_errors'),
-    LiquidityProvider: require('./revert_errors/zero-ex/liquidity_provider_revert_errors'),
-};
