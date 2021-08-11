fee281ca07fdb102531fff47fa817b9dabce6035
Simone Romano <simone.romano@noku.io>
2021-08-11 16:49:54 +0200

    fix

--- utils/test/revert_error_test.ts
+++ utils/test/revert_error_test.ts
@@ -54,81 +54,81 @@ describe('RevertError', () => {
         it('should equate two identical RevertErrors', () => {
             const revert1 = new StringRevertError(message);
             const revert2 = new StringRevertError(message);
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should equate two RevertErrors with missing fields', () => {
             const revert1 = new StringRevertError(message);
             const revert2 = new StringRevertError();
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should equate AnyRevertError with a real RevertError', () => {
             const revert1 = new StringRevertError(message);
             const revert2 = new AnyRevertError();
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should equate two revert errors with identical array fields', () => {
             const strings = ['foo', 'bar'];
             const revert1 = new ArrayRevertError(strings);
             const revert2 = new ArrayRevertError(strings);
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should not equate two revert errors with different sized array fields', () => {
             const strings = ['foo', 'bar'];
             const revert1 = new ArrayRevertError(strings);
             const revert2 = new ArrayRevertError(strings.slice(0, 1));
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should not equate two revert errors with different array field values', () => {
             const strings1 = ['foo', 'bar'];
             const strings2 = ['foo', 'baz'];
             const revert1 = new ArrayRevertError(strings1);
             const revert2 = new ArrayRevertError(strings2);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should equate two revert errors with identical fixed-size array fields', () => {
             const strings = ['foo', 'bar'];
             const revert1 = new FixedSizeArrayRevertError(strings);
             const revert2 = new FixedSizeArrayRevertError(strings);
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should not equate two revert errors with different sized fixed-size array fields', () => {
             const strings = ['foo', 'bar'];
             const revert1 = new FixedSizeArrayRevertError(strings);
             const revert2 = new FixedSizeArrayRevertError(strings.slice(0, 1));
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should not equate two revert errors with the wrong sized fixed-size array fields', () => {
             const strings = ['foo', 'bar', 'baz'];
             const revert1 = new FixedSizeArrayRevertError(strings);
             const revert2 = new FixedSizeArrayRevertError(strings);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should not equate two revert errors with different fixed-size array field values', () => {
             const strings1 = ['foo', 'bar'];
             const strings2 = ['foo', 'baz'];
             const revert1 = new FixedSizeArrayRevertError(strings1);
             const revert2 = new FixedSizeArrayRevertError(strings2);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should not equate a the same RevertError type with different values', () => {
             const revert1 = new StringRevertError(message);
             const revert2 = new StringRevertError(`${message}1`);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should not equate different RevertError types', () => {
             const revert1 = new StringRevertError(message);
             const revert2 = new DescendantRevertError(message);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
         it('should equate two `RawRevertError` types with the same raw data', () => {
             const revert1 = new RawRevertError('0x0123456789');
             const revert2 = new RawRevertError(revert1.encode());
-            expect(revert1.equals(revert2)).to.be.true();
+            expect(revert1.equals(revert2)).to.be.true;
         });
         it('should not equate two `RawRevertError` types with the different raw data', () => {
             const revert1 = new RawRevertError('0x0123456789');
             const revert2 = new RawRevertError(`${revert1.encode()}00`);
-            expect(revert1.equals(revert2)).to.be.false();
+            expect(revert1.equals(revert2)).to.be.false;
         });
     });
     describe('registering', () => {
@@ -154,7 +154,7 @@ describe('RevertError', () => {
         it('should decode an ABI encoded revert error', () => {
             const expected = new StringRevertError(message);
             const decoded = RevertError.decode(encoded);
-            expect(decoded.equals(expected)).to.be.true();
+            expect(decoded.equals(expected)).to.be.true;
         });
         it('should decode an unknown selector as a `RawRevertError`', () => {
             const _encoded = encoded.substr(0, 2) + '00' + encoded.substr(4);
