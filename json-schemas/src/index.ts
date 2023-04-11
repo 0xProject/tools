99bca0b1876326f7f134bad21acc1a5c221339f6
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2023-04-10 21:33:34 -0600

    refactor: Remove re-exports

--- json-schemas/src/index.ts
+++ json-schemas/src/index.ts
@@ -1,6 +1,2 @@
 export { SchemaValidator } from './schema_validator';
 export { schemas } from './schemas';
-export { Ajv } from 'ajv';
-import * as AJV from 'ajv';
-export { AJV };
-module.exports.AJV = AJV;
